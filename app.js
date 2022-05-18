5997e42b25d5843d4a5d6b4bfde43f1f6ee31cd6
Nightz <peerapat.ud@gmail.com>
2022-05-18 15:57:37 +0700

    Bind number buttons and update display

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
-// Basics operators
+const btnNum = Array.from(document.querySelectorAll(".btn-num"));
+const operationDisplay = document.querySelector(".display-operation");
+const currentDisplay = document.querySelector(".display-current");
+let displayNumber = 0;
+
+// Basics operators functions
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
-
 const operate = (operator, num1, num2) => operator(num1, num2);
+
+btnNum.forEach(num => {
+    num.addEventListener('click', updateCurrentnumber);
+})
+
+function updateCurrentnumber(event) {
+    if (currentDisplay.textContent === "0") {
+        displayNumber = event.target.getAttribute("data-number");
+        currentDisplay.textContent = displayNumber;
+    }
+    else {
+        displayNumber += event.target.getAttribute("data-number");
+        currentDisplay.textContent = displayNumber;
+    }
+}
+
+
+
+
+
